c75d98289c7efdd611a87b04e636fb9dc77e4c41
Mario Smalo <mario.smalo@gmail.com>
2023-10-16 11:53:29 +0200

    Read Json File

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Codium
 {
@@ -20,9 +10,23 @@ namespace Codium
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string? JsonFile = null;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Json File (*.json*)|*.json*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+                {
+                    JsonFile=streamReader.ReadToEnd();   
+                }
+            }
         }
     }
 }
