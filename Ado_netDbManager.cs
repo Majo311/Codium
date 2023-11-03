09cecadbb57b2cf86b4820daacb408001a11b08a
Mario Smalo <mario.smalo@gmail.com>
2023-11-03 16:27:35 +0100

    use paralerforreach to insert odds to db

--- Ado_netDbManager.cs
+++ Ado_netDbManager.cs
@@ -1,6 +1,7 @@
 ﻿using Codium.Models;
 using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
